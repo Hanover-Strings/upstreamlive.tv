4a801da81b27a02e2b23d96a19c92fe7abacf637
Christopher Goulet <fluxola500@gmail.com>
2018-08-30 08:47:31 -0400

    getting synced up -  mankeying with iframe width

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,8 +6,8 @@ const Body = () => (
   <div className='Body'>
     <Row xs='12'>
       <Col>
-      <div className='video-container'>
-        <iframe width="853" height="480"  src="https://www.youtube.com/embed/live_stream?channel=UCWK7I10nUKDRBv_stW8y61g" title='upstreamlive' frameBorder="0" allowFullScreen></iframe>
+      <div>
+        <iframe width="560" height="349"  src="https://www.youtube.com/embed/live_stream?channel=UCWK7I10nUKDRBv_stW8y61g" title='upstreamlive' frameBorder="0" allowFullScreen></iframe>
       </div>
       </Col>
     </Row>
