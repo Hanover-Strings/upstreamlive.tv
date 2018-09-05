1c32e589fd3f2140c1bea17d7262029d77860d62
Christopher Goulet <fluxola500@gmail.com>
2018-09-05 09:07:23 -0400

    placeholder for icecast server

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,6 +12,7 @@ const Footer = () => (
         </Col>
         <Col xs='12' sm='12' md='6'>
           <h3 style={{ color:"red", paddingTop:'12px'}}>Audio Version   <i class="fas fa-volume-up"></i></h3>
+          {/* fix this stream origin!!! */}
           <audio src="http://$STREAMURL:8000/stream"  autoplay="false" controls="true"></audio>
         </Col>
       </Row>      
