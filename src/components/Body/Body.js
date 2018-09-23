a42e3e6a5a6d05270f29827e4603f20049ed5594
Christopher Goulet <fluxola500@gmail.com>
2018-09-23 10:56:32 -0400

    rebuilt for playlist, post- 9/21

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,8 +6,8 @@ import ResponsiveEmbed from 'react-responsive-embed';
 const Body = () => (
         <Row className='Body'>
           <Col sm="12" md={{ size: 8, offset: 2 }} style={{paddingTop: "48px", paddingBottom: "48px"}}>
-          {/* <iframe width="853" height="480"  src="https://www.youtube.com/embed/live_stream?channel=UCWK7I10nUKDRBv_stW8y61g" title='upstreamlive' frameBorder="0" allowFullScreen></iframe>     */}
-          <ResponsiveEmbed src="https://www.youtube.com/embed/live_stream?channel=UCWK7I10nUKDRBv_stW8y61g" allowFullScreen />
+          {/* <ResponsiveEmbed src="https://www.youtube.com/embed/live_stream?channel=UCWK7I10nUKDRBv_stW8y61g" allowFullScreen />  THIS IS THE LIVESTREAM PLAYER, UNCOMMENT BEFORE BROADCAST!*/}
+          <ResponsiveEmbed src="https://www.youtube.com/embed/videoseries?list=PLZ_IPcIKCj-dZo4w8um_E16RTzVT6Wlur" allowFullScreen />
           </Col>
         </Row>
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,14 @@ class Header extends React.Component {
           <Col xs="12" horizontal="center" vertical='center' className='logo'>
             <embed src={logo} alt='logo' style={{ width: "45%", height: "auto", paddingTop: "24px" }} />
           </Col>
+          <Col xs='12'>
+            <h4 className='next-up'>
+              Tune In October 19 @ 7:30 for a livestream with <a href='https://www.facebook.com/dunehuntersounds/'>Dunehunter</a>
+            </h4>
+            <h4 className='next-up'>
+              Until then, enjoy some previous Upstreams...
+            </h4>
+          </Col>
         </Row>
     );
   }
